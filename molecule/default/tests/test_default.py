acb445a515c100051036f97a8b6c6be3753424e8
buldozer <123anice@mail.ru>
2019-03-19 16:56:53 +0300

    Add new rework

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,7 +4,6 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-MOLECULE_EPHEMERAL_DIRECTORY: /var/folders/wk/cwtmfyx9099gn_g744lt61bh0000gn/T/molecule/ansible-common/default
 
 # check if MongoDB is enabled and running
 def test_mongo_running_and_enabled(host):
@@ -16,10 +15,11 @@ def test_mongo_running_and_enabled(host):
 def test_config_file(File):
     config_file = File('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
-    assert config_file.exists
+    assert config_file.is_file
 
 #Listening port
 # http://testinfra.readthedocs.io/en/latest/modules.html#socket
 def test_port(host):
     host.socket("tcp://0.0.0.0:27017").is_listening
     assert host.socket.is_listening
+
